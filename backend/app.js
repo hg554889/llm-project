a5e9fc02fa2dd70dfc864b20e70d230d284ba4f4
hg554889 <hg554889@gmail.com>
2025-02-19 19:39:22 +0900

    add initial .npmignore and package.json files for various modules

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 connectDB();
 
 app.use("/", require("./routes/main"));

8e0eca73625edbf4edfae6251b73682f9a6d9015
김민서 <kimmich3737@gmail.com>
2025-03-08 16:43:00 +0900

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
-import './App.css'; // 스타일링을 위해 CSS 파일 추가
+import './index.css'; // 스타일링을 위해 CSS 파일 추가
 
 function App() {
   const [message, setMessage] = useState('');
@@ -23,14 +23,20 @@ function App() {
   return (
     <div className="container">
       <div className="header">
+        <i className="fa-sharp fa-solid fa-bars"></i>
+        <img src="/img/Job-A_icon.jpg" alt="Job Assistance Icon" />
         <h1>JOB-Assisitence</h1>
+        <i className="fa-solid fa-user"></i>
+        <i class="fa-solid fa-gear"></i>
       </div>
+
       <div className="content">
         <h1>{message}</h1>
       </div>
     </div>
   );
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

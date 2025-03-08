81ad66cdef3bde1147be73d8b9bd7d8fc8bbd903
김민서 <kimmich3737@gmail.com>
2025-03-08 17:04:40 +0900

    Add sidebar navigation and new pages for user, list, login, and sign-in

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
+import JobImage from '../src/img/Job-A_icon.png';
 import './index.css'; // 스타일링을 위해 CSS 파일 추가
 
 function App() {
   const [message, setMessage] = useState('');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,22 +22,59 @@ function App() {
     fetchData();
   }, []);
 
+  // 사이드바 열기/닫기 함수
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleJobClick =()=>{
+    //ai 자소서 페이지
+    window.location.href='/src/ai.js';
+  }
+  
+  const handleIndexClick=()=>{
+    //메인페이지로 이동
+    window.location.href='/src/index.js';
+  }
+
+  const handleUserClick = () => {
+    // 유저페이지로 이동
+    window.location.href = '/src/user.js';  
+  };
+
+  const handleEnvirClick=()=>{
+    //환경설정페이지로 이동동
+    window.location.href='/src/envir/js';
+  }
+
+
   return (
     <div className="container">
+    {/* 사이드바 */}
+    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+      <h2>Menu</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+    
+    <div className="container">
       <div className="header">
-        <i className="fa-sharp fa-solid fa-bars"></i>
-        <img src="/img/Job-A_icon.jpg" alt="Job Assistance Icon" />
-        <h1>JOB-Assisitence</h1>
-        <i className="fa-solid fa-user"></i>
-        <i class="fa-solid fa-gear"></i>
+        <i className="fa-sharp fa-solid fa-bars" onClick={toggleSidebar}></i>
+        <img src={JobImage} onClick={handleJobClick} />
+        <h1 onClick={handleIndexClick}>JOB-Assisitence</h1>
+        <i className="fa-solid fa-user" onClick={handleUserClick}></i>
+        <i class="fa-solid fa-gear" onClick={handleEnvirClick}></i>
       </div>
 
       <div className="content">
         <h1>{message}</h1>
       </div>
     </div>
+    </div>
   );
 }
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ client/src/user.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+

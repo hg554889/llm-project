4ef2afa48fecc01c92ab38169dd8f54ec7c839ae
김민서 <kimmich3737@gmail.com>
2025-03-16 03:13:01 +0900

    SignIn

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,8 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './Main.js';  // 메인 페이지
 import Login from './login.js';      // 로그인 페이지
 import SignIn from './signIn.js';    // 회원가입 페이지
-import UserPage from './user.js';    // 유저 페이지
+import FindInfo from './findInfo.js';  // 정보 찾기 페이지
 import EnvirPage from './envir.js';  // 환경 설정 페이지
+import UserMainPage from './UserMain.js';  // 유저 메인 페이지
+import MyPage from './MyPage.js';    // 마이 페이지(로그인 되면 아이콘 바뀌는데데)
 
 const Router = () => {
   return (
@@ -12,8 +14,10 @@ const Router = () => {
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signIn" element={<SignIn />} />
-      <Route path="/user" element={<UserPage />} />
+      <Route path="/findInfo" element={<FindInfo />} />
       <Route path="/envir" element={<EnvirPage />} />
+      <Route path="/userMain" element={<UserMainPage />} />
+      <Route path="/myPage" element={<MyPage />} />
     </Routes>
   );
 }
